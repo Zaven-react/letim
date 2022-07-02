655a7cfa63ac5457bd4095bb17e70f7f1104a3cf
Zaven-react <yegoryan@list.ru>
2022-07-02 16:21:43 +0400

    use makeStyle function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-
 import { Button } from '@material-ui/core';
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+  root: {
+    textAlign: "center",
+    marginTop:"100px",
+    position: props => props.position,
+  },
+});
 
-
-
-
-function App() {
-  return <Button color="primary">Hello World</Button>;
+function App(props) {
+  debugger
+  const classes = useStyles();
+ 
+  return <div className={classes.root} >
+    <Button color="primary">Hello World</Button>
+    
+    </div>;
 }
 
 export default App;

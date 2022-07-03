1882728124a97a3d37b70a7f6cde8a1962fe50c4
Zaven-react <yegoryan@list.ru>
2022-07-03 17:56:26 +0400

    ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import List  from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
 
 const useStyles = makeStyles({
   root: {
     textAlign: "center",
     marginTop:"100px",
-    position: props => props.position,
+    fontFamily:"cursive",  
+     position: props => props.position,
+    
   },
+  boot:{
+    backgroundColor:"red",
+  }
 });
 
+
+
 function App(props) {
-  debugger
-  const classes = useStyles();
  
+  const classes = useStyles();
+ let names = ["gyagoo","ruzik","abaz"];
+ let n = names.map((el)=>{
+  return <ListItem>{el}</ListItem>
+ })
+
   return <div className={classes.root} >
     <Button color="primary">Hello World</Button>
+    <List className={classes.boot}>
+      {n}     
+    </List>
+ 
     
     </div>;
 }

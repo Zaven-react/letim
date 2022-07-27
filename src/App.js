c258f8f1e5e08ebe29565b35656bc96cedbd96ca
Zaven-react <yegoryan@list.ru>
2022-07-27 14:10:48 +0400

    text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
+import { Button, TextField } from '@material-ui/core';
+import React, {useRef, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List  from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -13,25 +13,38 @@ const useStyles = makeStyles({
      position: props => props.position,
     
   },
-  boot:{
-    backgroundColor:"red",
-  }
+
 });
 
 
 
 function App(props) {
  
-  const classes = useStyles();
- let names = ["gyagoo","ruzik","abaz"];
- let n = names.map((el)=>{
-  return <ListItem>{el}</ListItem>
- })
+const classes = useStyles();
+
+ const [val,setVal] = useState("");
+ const [h3iArjeq,setH3iArjeq] = useState("");
+
+
+ const handleOnChange = (e) => {
+setVal(e.target.value)
+ }
+
+ const onClick = ()=>{  
+  setH3iArjeq(val) 
+  setVal("")
+}
+
 
   return <div className={classes.root} >
-    <Button color="primary">Hello World</Button>
-    <List className={classes.boot}>
-      {n}     
+    
+    <List>
+        
+    </List> 
+    <TextField onChange={handleOnChange} value={val}></TextField>
+    <Button color="primary" onClick={onClick}>Send</Button>
+    <List className={classes.boot} >
+       {h3iArjeq} 
     </List>
  
     
